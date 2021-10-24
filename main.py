94a1eaf99401e370f0c36a3538f8523a5d18ced4
vhsbabo <93032307+vhsbabo@users.noreply.github.com>
2021-10-24 17:44:06 +0200

    Beter gesorteerde blokken

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
+def mix():
+    global j
+    index = 0
+    while index <= len(lijst) - 1:
+        j = randint(0, len(lijst) - 1)
+        wissel(index, j)
+        index += 1
+
 def on_button_pressed_a():
-    lijst = mix([0,1,2,3, 4])
-    noten = [Note.A, Note.B, Note.C, Note.D, Note.E]
-    wis()
-    teken(lijst,noten)
+    mix()
+    tekenGrafiek()
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def tekenGrafiek():
+    basic.clear_screen()
+    index2 = 0
+    while index2 <= len(lijst) - 1:
+        led.plot(index2, 4 - lijst[index2])
+        music.play_tone(noten[lijst[index2]], music.beat(BeatFraction.WHOLE))
+        index2 += 1
+def wissel(a: number, b: number):
+    global dummy
+    dummy = lijst[a]
+    lijst[a] = lijst[b]
+    lijst[b] = dummy
+
 def on_button_pressed_b():
-    wis()
+    sorteer()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def mix(list2: List[number]):
-    for index in range(len(list2) - 1):
-        i = randint(0, len(list2) - 1)
-        dummy = list2[i]
-        list2[i] = list2[index]
-        list2[index] = dummy
-    return list2
-
-def teken(lijst: List[number], noten: List[Note]):
-    for index in range(len(lijst)):
-        print("%s %s"%(index, lijst[index]))
-        led.plot(index, lijst[index])
-        music.play_tone(noten[lijst[index]], music.beat())
-
-def wis():
-    for i in range(5):
-        for j in range(5):
-            led.unplot(i, j)
+def sorteer():
+    index3 = 0
+    while index3 <= len(lijst) - 2:
+        if lijst[index3] > lijst[index3 + 1]:
+            wissel(index3, index3 + 1)
+        index3 += 1
+    tekenGrafiek()
+dummy = 0
+j = 0
+lijst: List[number] = []
+noten: List[number] = []
+noten = [262, 294, 330, 349, 392]
+lijst = [0, 1, 2, 3, 4]
